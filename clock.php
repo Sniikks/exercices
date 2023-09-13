2190f7f3434932b293fd80cf5b95d2a1f90d96be
Sniikks <snikspro@hotmail.com>
2023-09-13 16:29:05 +0200

    Commit all

--- clock.php
+++ clock.php
@@ -7,7 +7,48 @@
     <title>Clock.php</title>
 </head>
 <body>
-    <!-- Faite une horloge sois numérique sois analogique, on doit pouvoir changer l'heure avec un formulaire-->
+
+<p id="minuteur">00: 00: 00</p>
+    <form action="" method="post">
+        <label for="heure"> Heure(s) :</label>
+        <input type="number" name="heure" id="heure" value=0 min=0 max=9999999 lenght=2>
+        <label for="minute"> Minute(s) :</label>
+        <input type="number" name="minute" id="minute" value=0 min=0 max=59>
+        <label for="seconde"> Seconde(s) :</label>
+        <input type="number" name="seconde" id="seconde" value=0 min=0 max=59>
+        <input type="submit" value="Submit">
+    </form>
+
+<?php
+ 
+
+echo '<script>
+    var heure =' .(!empty($_POST["heure"]) ? $_POST["heure"] : '0') .'
+    var minute =' .(!empty($_POST["minute"]) ? $_POST["minute"] : '0') .'
+    var seconde =' .(!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') .'
+    setInterval(function() {
+        document.getElementById("minuteur").innerHTML= 
+        `${(heure < 10 ? "0" : "") + heure}:
+         ${(minute < 10 ? "0" : "") + minute}:
+         ${(seconde < 10 ? "0" : "") + seconde}`
+        if (seconde <= 0) return
+        seconde--
+        if (seconde == 0)
+        if (minute > 0) {
+            seconde = 59
+            minute--
+        } else {
+            if (heure > 0) {
+                heure--
+                minute = 59
+                seconde = 59
+            }
+        }
+}, 1000)
+
+</script>'
+
+?>
 
 <h3> Date et Horaire du jour </h3>
 <?php

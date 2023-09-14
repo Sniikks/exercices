23e6e95afc7d47dec59bcdf598587f024aa85bb4
Sniikks <snikspro@hotmail.com>
2023-09-14 10:10:30 +0200

    Commit all

--- clock.php
+++ clock.php
@@ -9,46 +9,44 @@
 <body>
 
 <p id="minuteur">00: 00: 00</p>
-    <form action="" method="post">
-        <label for="heure"> Heure(s) :</label>
-        <input type="number" name="heure" id="heure" value=0 min=0 max=9999999 lenght=2>
-        <label for="minute"> Minute(s) :</label>
-        <input type="number" name="minute" id="minute" value=0 min=0 max=59>
-        <label for="seconde"> Seconde(s) :</label>
-        <input type="number" name="seconde" id="seconde" value=0 min=0 max=59>
-        <input type="submit" value="Submit">
-    </form>
+<form action="" method="post">
+    <label for="heure"> Heure(s) :</label>
+    <input type="number" name="heure" id="heure" value="0" min="0" max="9999999">
+    <label for="minute"> Minute(s) :</label>
+    <input type="number" name="minute" id="minute" value="0" min="0" max="59" maxlength="2">
+    <label for="seconde"> Seconde(s) :</label>
+    <input type="number" name="seconde" id="seconde" value="0" min="0" max="59" maxlength="2">
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+</form>
 
 <?php
- 
-
 echo '<script>
-    var heure =' .(!empty($_POST["heure"]) ? $_POST["heure"] : '0') .'
-    var minute =' .(!empty($_POST["minute"]) ? $_POST["minute"] : '0') .'
-    var seconde =' .(!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') .'
+    var heure =' . (!empty($_POST["heure"]) ? $_POST["heure"] : '0') . ';
+    var minute =' . (!empty($_POST["minute"]) ? $_POST["minute"] : '0') . ';
+    var seconde =' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') . ';
     setInterval(function() {
-        document.getElementById("minuteur").innerHTML= 
+        document.getElementById("minuteur").innerHTML = 
         `${(heure < 10 ? "0" : "") + heure}:
          ${(minute < 10 ? "0" : "") + minute}:
-         ${(seconde < 10 ? "0" : "") + seconde}`
-        if (seconde <= 0) return
-        seconde--
-        if (seconde == 0)
-        if (minute > 0) {
-            seconde = 59
-            minute--
-        } else {
-            if (heure > 0) {
-                heure--
-                minute = 59
-                seconde = 59
+         ${(seconde < 10 ? "0" : "") + seconde}`;
+        if (seconde <= 0 && minute <= 0 && heure <= 0) return;
+        seconde--;
+        if (seconde < 0) {
+            seconde = 59;
+            minute--;
+            if (minute < 0) {
+                minute = 59;
+                heure--;
             }
         }
-}, 1000)
-
-</script>'
+    }, 1000);
+    
+</script>';
 
 ?>
+
+<!-- ------------------------------ -->
 
 <h3> Date et Horaire du jour </h3>
 <?php

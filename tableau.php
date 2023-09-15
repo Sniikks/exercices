076b8f3f2fdaeb8a943bcb1882e516dc81d5005c
Sniikks <snikspro@hotmail.com>
2023-09-15 09:39:02 +0200

    Commit all

--- tableau.php
+++ tableau.php
@@ -8,77 +8,75 @@
 <body>
 <table border="1">
         <tr>
-            <td colspan="6">Jeux Olympiques de Rio 2016</td>
+            <td class="texte2" colspan="6">Jeux Olympiques de Rio 2016</td>
         </tr>
         <tr>
-            <th></th>
-            <th>Pays</th>
-            <th><img src="./img/or.png"></th>
-            <th><img src="./img/argent.png"></th>
-            <th><img src="./img/bronze.png"></th>
-            <th><img src="./img/total.png"></th>
+            <td class="texte1"></td>
+            <td class="texte1">Pays</td>
+            <td class="texte1"><img class="img1" src="./img/or.png"></td>
+            <td class="texte1"><img class="img1" src="./img/argent.png"></td>
+            <td class="texte1"><img class="img1" src="./img/bronze.png"></td>
+            <td class="texte1"><img class="img1" src="./img/total.png"></td>
         </tr>
         <tr>
-            <th>1</th>
-            <th><img src="./img/usa.png">USA</th>
-            <th>46</th>
-            <th>37</th>
-            <th>38</th>
-            <th>121</th>
+            <td class="texte4">1</td>
+            <td class="pays1"><img class="img2" src="./img/usa.png">USA</td>
+            <td>46</td>
+            <td>37</td>
+            <td>38</td>
+            <td class="texte3">121</td>
         </tr>
         <tr>
-            <td>2</td>
-            <th><img src="./img/grande_bretagne.png">Grande-Bretagne</td>
+            <td class="texte4">2</td>
+            <td class="pays1"><img class="img2" src="./img/grande_bretagne.png">Grande-Bretagne</td>
             <td>27</td>
             <td>23</td>
             <td>17</td>
-            <td>67</td>
+            <td class="texte3">67</td>
         </tr>
         <tr>
-            <td>3</td>
-            <th><img src="./img/chine.png">Chine</td>
+            <td class="texte4">3</td>
+            <td class="pays1"><img class="img2" src="./img/chine.png">Chine</td>
             <td>26</td>
             <td>18</td>
             <td>26</td>
-            <td>70</td>
+            <td class="texte3">70</td>
         </tr>
         <tr>
-            <td>4</td>
-            <th><img src="./img/russie.png">Russie</td>
+            <td class="texte4">4</td>
+            <td class="pays1"><img class="img2" src="./img/russie.png">Russie</td>
             <td>19</td>
             <td>18</td>
             <td>19</td>
-            <td>56</td>
+            <td class="texte3">56</td>
         </tr>
         <tr>
-            <td>5</td>
-            <th><img src="./img/allemagne.png">Allemagne</td>
+            <td class="texte4">5</td>
+            <td class="pays1"><img class="img2" src="./img/allemagne.png">Allemagne</td>
             <td>17</td>
             <td>10</td>
             <td>15</td>
-            <td>42</td>
+            <td class="texte3">42</td>
         </tr>
         <tr>
-            <td>6</td>
-            <th><img src="./img/japon.png">Japon</td>
+            <td class="texte4">6</td>
+            <td class="pays1"><img class="img2" src="./img/japon.png">Japon</td>
             <td>12</td>
             <td>8</td>
             <td>21</td>
-            <td>41</td>
+            <td class="texte3">41</td>
         </tr>
         <tr>
-            <td>7</td>
-            <th><img src="./img/france.png">France</td>
+            <td class="texte4">7</td>
+            <td class="pays1"><img class="img2" src="./img/france.png">France</td>
             <td>10</td>
             <td>18</td>
             <td>14</td>
-            <td>42</td>
+            <td class="texte3">42</td>
         </tr>
         <tr>
-            <td></td>
-            <td>Ce classement est limité aux 7 premières nations</td>
-            </tr>
+            <td class="texte2" colspan="6">Ce classement est limité aux 7 premières nations</td>
+        </tr>
     </table>
-    <hr>
 </body>
 </html>

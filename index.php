dc52f2965ab4273bb0c1ac4bd484841dcd9cb73e
Sniikks <snikspro@hotmail.com>
2023-09-15 11:04:13 +0200

    Commit all

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once('db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index.php</title>
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./Projet.Phoenix/style.css">
 
     
 </head>

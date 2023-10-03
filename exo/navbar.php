db4cc40a30d2ca664fd65cd37a9ed7df424fbcc8
Sniikks <snikspro@hotmail.com>
2023-10-03 15:40:26 +0200

    Commit all

--- exo/navbar.php
+++ exo/navbar.php
@@ -15,20 +15,21 @@
             <li><a href="">Blocks<box-icon name="chevron-down"></box-icon></a></li>
             <li><a href="">Docs<box-icon name="chevron-down"></box-icon></a></li>
         </ul>
-        <ul id="slide">
-            <li><a href="">Pages</a></li>
-            <li><a href="">Account</a></li>
-            <li><a href="">Blocks</a></li>
-            <li><a href="">Docs</a></li>
-        </ul>
+        
         <button class="kinder">BUY NOW</button>
         <box-icon id="menu" name="menu" size="25px"></box-icon>
     </nav>
+    <ul id="slide">
+        <li><a href="">Pages</a></li>
+        <li><a href="">Account</a></li>
+        <li><a href="">Blocks</a></li>
+        <li><a href="">Docs</a></li>
+    </ul>
     <script>
         let menu = document.getElementById('menu')
         menu.addEventListener('click', function() {
             let list = document.getElementById('slide')
-            list.style.display = 'block';
+            list.style.display = 'flex';
         })
 
     </script>

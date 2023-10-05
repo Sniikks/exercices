cf99ca02d1fb210659f1d89d97ba266131a5078d
Sniikks <snikspro@hotmail.com>
2023-10-05 13:30:38 +0200

    Commit all

---
+++ cours/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">    
+    <link rel="stylesheet" href="../style/footer.css">
+    <title>Footer</title>
+</head>
+<body>
+    <footer>
+        <ul>        
+            <h2>Arche de Noé</h2>
+
+            <li><a href="#">Autruche</a></li>
+            <li><a href="#">Canary</a></li>
+            <li><a href="#">Cerf</a></li>
+            <li><a href="#">Faisan</a></li>
+            <li><a href="#">Dromadaire</a></li>
+            <li><a href="#">Peruche</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
--- exo/navbar2.php
+++ exo/navbar2.php
@@ -13,31 +13,39 @@
             <box-icon name="world" color="#fff" size="30px"></box-icon>
             <span>Mondial World</span>
         </div>
-        <ul>
-            <li>About</li>
-            <li>Services</li>
-            <li>Testimonials</li>
+        <ul>                 
+            <li>Chat</li>
+            <li>Chien</li>
+            <li>Autruche</li>
         </ul> 
         <div> 
             <box-icon name="github" type="logo" color="#fff" size="30px"></box-icon>
             <box-icon id='menu' name="menu" color="#fff" size="30px"></box-icon>
+            <box-icon name="x" type="icon" color="#fff" size='30px'></box-icon>
         </div>
     </nav>
     <script>
         $('#menu').click(function() {
-            if ($('nav ul').is(':visible')) {
-                $('nav ul').animate({
-                    opacity: 0,
-                }, 500, function() {
-                    $('nav ul').hide()
-                })
-            } else {
-                $('nav ul').show()
-                $('nav ul').animate({
-                    opacity: 1,
-                    top: '10px'
-                }, 500)
-                
+            if ($('nav ul').is(':hidden')) {                    
+                $('nav').animate({
+                    bottom: 0,
+                    backgroundColor: '#000D1A'
+                }, 1000, function(){
+                    $('nav ul').css({'display': 'flex'})                
+                })                
+                $('nav').addClass('SwitchNav')
+            }
+        })
+        $('box-icon[name="x"]').click(function() {
+            if ($('nav ul').is(':visible')) {                     
+                $('nav').animate({
+                    bottom: '93.5%',
+                    backgroundColor: '#000'
+                }, 1000, function() {
+                    $('nav').removeClass('SwitchNav')
+
+                })                         
+                $('nav ul').hide()
             }
         })
     </script>

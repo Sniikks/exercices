3261798b7112ad40887aceaef2c85eb0f2ea8b0f
Sniikks <snikspro@hotmail.com>
2024-01-08 16:23:11 +0100

    Commit all

---
+++ velo/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Logo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="" alt="LOGO">
+            <ul>
+                <li class="selected"><a href="">HOME PAGE</a></li>
+                <li><a href="">COURSE</a></li>
+                <li><a href="">DYNAMIC</a></li>
+                <li><a href="">CONTACT US</a></li>
+            </ul>
+            <div>
+                <box-icon name="menu"></box-icon>
+                <box-icon name="search"></box-icon>
+                <input type="search" name="search" id="search">
+            </div>
+        </nav>
+    </header>
+    <section>
+        <h2>Welcome to Vietnam</h2>
+        <h3>Welcome to beautiful Nha Trang</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum molestias natus sapiente. Facilis ab, possimus ex nobis accusantium veritatis temporibus eius corrupti est, excepturi rerum deserunt totam reiciendis pariatur!</p>
+        <button> See More >></button>
+        <div>
+            <box-icon name="facebook"></box-icon>
+            <box-icon name="youtube"></box-icon>
+            <box-icon name="linkedin"></box-icon>
+        </div>
+    </section>
+</body>
+</html>

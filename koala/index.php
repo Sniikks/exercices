74f42baed7a74ca1869e6542302a8d4059b8d619
Sniikks <snikspro@hotmail.com>
2024-01-09 15:35:20 +0100

    Commit all

---
+++ koala/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Koala</title>
+</head>
+<body>
+    <div class="boite">
+        <div class="oreilleGauche">
+            <div class="oreilleGaucheInt"></div>
+        </div>
+        
+        <div class="oreilleDroite">
+            <div class="oreilleDroiteInt"></div>
+        </div>
+        
+        <div class="visage">
+            <div class="oeilGauche">
+                <div class="pupilleGauche"></div>
+                <div class="paupiereGauche"></div>
+            </div>
+            <div class="oeilDroit">
+                <div class="pupilleDroite"></div>
+                <div class="paupiereDroite"></div>
+            </div>
+            <div class="nez"></div>
+        </div>
+    </div>
+</body>
+</html>
--- velo/index.php
+++ velo/index.php
@@ -11,12 +11,12 @@
         <nav>
             <img src="" alt="LOGO">
             <ul>
-                <li class="selected"><a href="">HOME PAGE</a></li>
-                <li><a href="">COURSE</a></li>
-                <li><a href="">DYNAMIC</a></li>
-                <li><a href="">CONTACT US</a></li>
+                <li class="selected"><a href="">HOME PAGE <div></div></a></li>
+                <li><a href="">COURSE <div></div></a></li>
+                <li><a href="">DYNAMIC <div></div></a></li>
+                <li><a href="">CONTACT US <div></div></a></li>
             </ul>
-            <div>
+            <div class="input">
                 <box-icon name="menu"></box-icon>
                 <box-icon name="search"></box-icon>
                 <input type="search" name="search" id="search">
@@ -24,14 +24,14 @@
         </nav>
     </header>
     <section>
-        <h2>Welcome to Vietnam</h2>
-        <h3>Welcome to beautiful Nha Trang</h3>
+        <h1>Welcome to Vietnam</h1>
+        <h2>Welcome to beautiful Nha Trang</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum molestias natus sapiente. Facilis ab, possimus ex nobis accusantium veritatis temporibus eius corrupti est, excepturi rerum deserunt totam reiciendis pariatur!</p>
-        <button> See More >></button>
+        <button> SEE MORE >></button>
         <div>
-            <box-icon name="facebook"></box-icon>
-            <box-icon name="youtube"></box-icon>
-            <box-icon name="linkedin"></box-icon>
+            <box-icon name="facebook" type="logo" color="white" size="1.4em"></box-icon>
+            <box-icon name="youtube" type="logo" color="white" size="1.4em"></box-icon>
+            <box-icon name="linkedin" type="logo" color="white" size="1.4em"></box-icon>
         </div>
     </section>
 </body>
